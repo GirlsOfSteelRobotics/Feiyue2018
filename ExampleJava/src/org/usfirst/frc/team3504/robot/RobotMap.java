15d8917a493774a8c916739a497a9506bdf52b8e
Michelle Guo <Runbo@Michelle-PC>
2018-07-13 10:12:04 -0400

    cleaned up comments

--- ExampleJava/src/org/usfirst/frc/team3504/robot/RobotMap.java
+++ ExampleJava/src/org/usfirst/frc/team3504/robot/RobotMap.java
@@ -14,16 +14,7 @@ package org.usfirst.frc.team3504.robot;
  * floating around.
  */
 public class RobotMap {
-	// For example to map the left and right motors, you could define the
-	// following variables to use with your drivetrain subsystem.
-	// public static int leftMotor = 1;
-	// public static int rightMotor = 2;
 
-	// If you are using multiple modules, make sure to define both the port
-	// number and the module. For example you with a rangefinder:
-	// public static int rangefinderPort = 1;
-	// public static int rangefinderModule = 1;
-	
 	public static final int LEFT_MASTER = 1;
 	public static final int LEFT_FOLLOW_A = 2;
 	public static final int LEFT_FOLLOW_B = 3;
